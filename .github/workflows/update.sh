a2125c796984b1ba8c77b6705343998b17e2cf82
Hrishikesh-Kalyanaraman <92749037+Hrishikesh-Kalyanaraman@users.noreply.github.com>
2022-07-16 19:03:40 -0500

    modified update.sh (commit and push sections)
    This should ensure that CI workflow is run when update.sh is finished because of the git remote set url line ( https://github.com/rhaas80/einsteintoolkit/blob/master/.github/workflows/docs.yml#L82 shows how to use tokens to push.)

--- .github/workflows/update.sh
+++ .github/workflows/update.sh
@@ -16,10 +16,15 @@ for m in $(git diff --cached --name-only) ; do  (
 ) done
 
 if ! git diff --cached --exit-code --quiet ; then
+  git config --global user.email "maintainters@einsteintoolkit.org"
+  git config --global user.name "GitHub updater"
   git commit -q -F - <<EOF
 updated submodules
 $(git diff --cached --submodule=log)
 EOF
+  # could use GITHUB_SERVER_URL but it requires inserting
+  # text after the https:// in it which is annoying
+  git remote set-url --push origin "https://x-access-token:${PERSONAL_TOKEN}@github.com/${GITHUB_REPOSITORY}"
   git push
 fi
 
